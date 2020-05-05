b4ceed2eb4065c928a7688660878499c66c5e958
Orlando Flaschka <flaschkaorlando@gmail.com>
2020-05-05 07:34:42 -0300

    un par de vistas mas linkeadas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,9 +22,12 @@ namespace Congelistos
     {
 
         public Uri vistaClientes = new Uri(@".\Paginas\Clientes.xaml", UriKind.Relative);
+        public Uri vistaCompras = new Uri(@".\Paginas\Compras.xaml", UriKind.Relative);
+        public Uri vistaMateriaPrima = new Uri(@".\Paginas\MateriaPrima.xaml", UriKind.Relative);
         public Uri vistaPedidos = new Uri(@".\Paginas\Pedidos.xaml", UriKind.Relative);
         public Uri vistaProveedores = new Uri(@".\Paginas\Proveedores.xaml", UriKind.Relative);
-        public Uri vistaInsumos = new Uri(@".\Paginas\Insumos.xaml", UriKind.Relative);
+        public Uri vistaProductos = new Uri(@".\Paginas\Productos.xaml", UriKind.Relative);
+
 
         public MainWindow()
         {
@@ -43,16 +46,28 @@ namespace Congelistos
             framePrincipal.Source = vistaPedidos;
         }
 
-        private void botonInsumos_Click(object sender, RoutedEventArgs e)
-        {
-            vGridGeneral.Background.Opacity = 0.3;
-            framePrincipal.Source = vistaInsumos;
-        }
-
         private void botonProveedores_Click(object sender, RoutedEventArgs e)
         {
             vGridGeneral.Background.Opacity = 0.3;
             framePrincipal.Source = vistaProveedores;
+        }
+
+        private void botonProductos_Click(object sender, RoutedEventArgs e)
+        {
+            vGridGeneral.Background.Opacity = 0.3;
+            framePrincipal.Source = vistaProductos;
+        }
+
+        private void botonMateriaPrima_Click(object sender, RoutedEventArgs e)
+        {
+            vGridGeneral.Background.Opacity = 0.3;
+            framePrincipal.Source = vistaMateriaPrima;
+        }
+
+        private void botonCompras_Click(object sender, RoutedEventArgs e)
+        {
+            vGridGeneral.Background.Opacity = 0.3;
+            framePrincipal.Source = vistaCompras;
         }
     }
 }
